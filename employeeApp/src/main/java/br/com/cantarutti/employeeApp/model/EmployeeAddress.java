6f681c4a87fbf7be754edc20cd4299d30c6157b2
cantaruttim <cantaruttim@outlook.com>
2025-10-25 16:36:59 -0300

    employee address class

--- employeeApp/src/main/java/br/com/cantarutti/employeeApp/model/EmployeeAddress.java
+++ employeeApp/src/main/java/br/com/cantarutti/employeeApp/model/EmployeeAddress.java
@@ -1,8 +1,45 @@
 package br.com.cantarutti.employeeApp.model;
 
+import java.util.Objects;
+
 public class EmployeeAddress {
 
     private String registrationNumber;
     private String address;
     private String homeType;
+
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getHomeType() {
+        return homeType;
+    }
+
+    public void setHomeType(String homeType) {
+        this.homeType = homeType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof EmployeeAddress that)) return false;
+        return Objects.equals(getRegistrationNumber(), that.getRegistrationNumber()) && Objects.equals(getAddress(), that.getAddress()) && Objects.equals(getHomeType(), that.getHomeType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRegistrationNumber(), getAddress(), getHomeType());
+    }
 }
