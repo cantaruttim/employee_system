0a18a0c4caafc0833b3a808492d1d23a9d5afa4a
cantaruttim <cantaruttim@outlook.com>
2025-10-25 16:37:58 -0300

    getters and setters

--- employeeApp/src/main/java/br/com/cantarutti/employeeApp/model/EmployeeFinanceInfo.java
+++ employeeApp/src/main/java/br/com/cantarutti/employeeApp/model/EmployeeFinanceInfo.java
@@ -1,11 +1,36 @@
 package br.com.cantarutti.employeeApp.model;
 
+import java.util.Objects;
+
 public class EmployeeFinanceInfo {
 
     private String registrationNumber;
     private String roleName;
     private Double baseSalary;
 
+    public String getRegistrationNumber() {
+        return registrationNumber;
+    }
+
+    public void setRegistrationNumber(String registrationNumber) {
+        this.registrationNumber = registrationNumber;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
 
     public EmployeeFinanceInfo (String reg, String roleName, Double baseSalary) {
         this.registrationNumber = reg;
@@ -19,4 +44,14 @@ public class EmployeeFinanceInfo {
         return this.baseSalary = (baseSalary + (baseSalary * taxIncrease));
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof EmployeeFinanceInfo that)) return false;
+        return Objects.equals(getRegistrationNumber(), that.getRegistrationNumber()) && Objects.equals(getRoleName(), that.getRoleName()) && Objects.equals(getBaseSalary(), that.getBaseSalary());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getRegistrationNumber(), getRoleName(), getBaseSalary());
+    }
 }
