1f960cba7bb15f210ae545cc926fa7b2d39059b7
Matheus Cantarutti <matheuscantarutti@Matheuss-MacBook-Air.local>
2025-11-29 22:32:30 -0300

    repository and jpa

--- employeeApp/src/main/java/br/com/cantarutti/controller/EmployeeController.java
+++ employeeApp/src/main/java/br/com/cantarutti/controller/EmployeeController.java
@@ -1,7 +1,10 @@
 package br.com.cantarutti.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController()
+@RequestMapping("/employee")
 public class EmployeeController {
-
-
-
+    // return all employee data from other classes
 }
---
+++ employeeApp/src/main/java/br/com/cantarutti/data/dto/EmployeeDTO.java
@@ -0,0 +1,7 @@
+package br.com.cantarutti.data.dto;
+
+public class EmployeeDTO {
+
+
+
+}
