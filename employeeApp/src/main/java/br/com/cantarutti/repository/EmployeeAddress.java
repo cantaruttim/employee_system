69f99ebbf27386dcc274b6b29670ca88dd51ae9a
cantaruttim <cantaruttim@outlook.com>
2025-11-30 10:19:48 -0300

    build interfaces

---
+++ employeeApp/src/main/java/br/com/cantarutti/repository/EmployeeAddress.java
@@ -0,0 +1,8 @@
+package br.com.cantarutti.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeAddress extends JpaRepository<EmployeeAddress, String> {
+
+
+}
