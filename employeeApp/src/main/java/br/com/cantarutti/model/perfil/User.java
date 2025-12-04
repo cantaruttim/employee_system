3b4d358a068ba3e0671e21da271688e9fb9312f1
cantaruttim <cantaruttim@outlook.com>
2025-12-04 19:30:35 -0300

    updated User

--- employeeApp/src/main/java/br/com/cantarutti/model/perfil/User.java
+++ employeeApp/src/main/java/br/com/cantarutti/model/perfil/User.java
@@ -33,6 +33,19 @@ public class User {
         this.userPerfil = definirPerfilAutomaticamente();
     }
 
+    public UUID getUserId() { return id; }
+    public String getUserName() { return userName; }
+    public String getUserEmail() { return userEmail; }
+    public String getUserPassword() { return userPassord; }
+    public Perfil getUserPerfil() { return userPerfil; }
+
+    public void setUserId(UUID id) { this.id = id; }
+    public void setUserName(String userName) { this.userName = userName; }
+    public void setUserEmail(String userEmail) { this.userEmail = userEmail; }
+    public void setUserPassword(String userPassord) { this.userPassord = userPassord; }
+    public void setUserPerfil(Perfil userPerfil) { this.userPerfil = userPerfil; }
+
+
     private Perfil definirPerfilAutomaticamente() {
         for (RegraPerfil regra : regras) {
             Perfil resultado = regra.verificar(this);
@@ -42,9 +55,6 @@ public class User {
         }
         return Perfil.USER; // padrão
     }
-
-    public String getUserName() { return userName; }
-    public String getUserEmail() { return userEmail; }
 
     interface RegraPerfil {
         Perfil verificar(User user);
