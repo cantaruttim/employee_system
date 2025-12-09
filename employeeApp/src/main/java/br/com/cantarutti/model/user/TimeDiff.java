f3060c397455c873b3c754a163b42289f98e45e5
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-12-08 21:09:45 -0300

    Update TimeDiff.java

--- employeeApp/src/main/java/br/com/cantarutti/model/user/TimeDiff.java
+++ employeeApp/src/main/java/br/com/cantarutti/model/user/TimeDiff.java
@@ -3,30 +3,18 @@ package br.com.cantarutti.model.user;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
 
-public class TimeDiff {
+public class TimeDiff extends TimeWorked {
 
-    private long firstMarker;
-    private long forthMarker;
-
-    public TimeDiff() {}
-
-    public TimeDiff(long firstMarker) {
-        this.firstMarker = firstMarker;
-    }
+    public TimeDiff(long hour, long minute) {}
     
-    public TimeDiff(long firstMarker, long forthMarker) {
-        this.firstMarker = firstMarker;
-        this.forthMarker = forthMarker;
-    }
-
-    public long getfirstMarker() { return firstMarker; }
-    public long getforthMarker() { return forthMarker; }
-
 
     public TimeDiff calculateDiffTime(LocalDateTime start, LocalDateTime end) {
+
+        TimeWorked tw = new TimeWorked();
+
         // verify with the worker worked 8 hour/day
-        long hour = ChronoUnit.HOURS.between(start, end);
-        long minute = ChronoUnit.MINUTES.between(start, end);
+        long hour = ChronoUnit.HOURS.between(tw.getFirstMarker(), tw.getForthMarker());
+        long minute = ChronoUnit.MINUTES.between(tw.getFirstMarker(), tw.getForthMarker());
 
         return new TimeDiff(hour, minute);
     }
