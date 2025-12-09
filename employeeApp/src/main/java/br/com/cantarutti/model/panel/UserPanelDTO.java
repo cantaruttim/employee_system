2105bc60ccdb285847882c1e1cf72764b9031fae
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2025-12-08 21:11:21 -0300

    Add UserPanelDTO class

--- employeeApp/src/main/java/br/com/cantarutti/model/panel/UserPanelDTO.java
+++ employeeApp/src/main/java/br/com/cantarutti/model/panel/UserPanelDTO.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class UserPanel {
+public class UserPanelDTO {
     // class responsable to get user information
 
     private UUID id;
@@ -29,48 +29,20 @@ public class UserPanel {
         return registrationNumber;
     }
 
-    public void setRegistrationNumber(User registrationNumber) {
-        this.registrationNumber = registrationNumber;
-    }
-
     public LocalDate getFirstPoint() {
         return firstPoint;
-    }
-
-    public void setFirstPoint(LocalDate firstPoint) {
-        this.firstPoint = firstPoint;
     }
 
     public LocalDate getSecondPoint() {
         return secondPoint;
     }
 
-    public void setSecondPoint(LocalDate secondPoint) {
-        this.secondPoint = secondPoint;
-    }
-
     public LocalDate getThirdPoint() {
         return thirdPoint;
     }
 
-    public void setThirdPoint(LocalDate thirdPoint) {
-        this.thirdPoint = thirdPoint;
-    }
-
     public LocalDate getFourthPoint() {
         return fourthPoint;
-    }
-
-    public void setFourthPoint(LocalDate fourthPoint) {
-        this.fourthPoint = fourthPoint;
-    }
-
-    public TimeDiff quantityWorked(
-            LocalDateTime firstPoint,
-            LocalDateTime fourthPoint
-    ) {
-        TimeDiff timeDiff = new TimeDiff();
-        return timeDiff.calculateDiffTime(firstPoint, fourthPoint);
     }
 
 }
