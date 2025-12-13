fc1893499479ff6e8f4921d8143c919976c4ae03
cantaruttim <cantaruttim@outlook.com>
2025-12-13 16:23:19 -0300

    create EmployeeCordinationDTO

--- employeeApp/src/main/java/br/com/cantarutti/config/mapper/EmployeeMapper.java
+++ employeeApp/src/main/java/br/com/cantarutti/config/mapper/EmployeeMapper.java
@@ -46,6 +46,8 @@ public class EmployeeMapper {
             employee.setFinanceInfo(finance);
         }
 
+        // Coordination
+
         return employee;
     }
 
--- employeeApp/src/main/java/br/com/cantarutti/data/employeeDTO/EmployeeCoordinationDTO.java
+++ employeeApp/src/main/java/br/com/cantarutti/data/employeeDTO/EmployeeCoordinationDTO.java
@@ -2,6 +2,54 @@ package br.com.cantarutti.data.employeeDTO;
 
 public class EmployeeCoordinationDTO {
 
-    
+    private String rolePositionName;
+    private String employeeOnOficial;
+    private String userNameSystem;
+    private String userPasswordSystem;
 
+    public EmployeeCoordinationDTO() {}
+
+    public EmployeeCoordinationDTO(
+            String rolePositionName,
+            String employeeOnOficial,
+            String userNameSystem,
+            String userPasswordSystem
+    ) {
+        this.rolePositionName = rolePositionName;
+        this.employeeOnOficial = employeeOnOficial;
+        this.userNameSystem = userNameSystem;
+        this.userPasswordSystem = userPasswordSystem;
+    }
+
+    public String getRolePositionName() {
+        return rolePositionName;
+    }
+
+    public void setRolePositionName(String rolePositionName) {
+        this.rolePositionName = rolePositionName;
+    }
+
+    public String getEmployeeOnOficial() {
+        return employeeOnOficial;
+    }
+
+    public void setEmployeeOnOficial(String employeeOnOficial) {
+        this.employeeOnOficial = employeeOnOficial;
+    }
+
+    public String getUserNameSystem() {
+        return userNameSystem;
+    }
+
+    public void setUserNameSystem(String userNameSystem) {
+        this.userNameSystem = userNameSystem;
+    }
+
+    public String getUserPasswordSystem() {
+        return userPasswordSystem;
+    }
+
+    public void setUserPasswordSystem(String userPasswordSystem) {
+        this.userPasswordSystem = userPasswordSystem;
+    }
 }
